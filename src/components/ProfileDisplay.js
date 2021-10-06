bd2edeb7c73d085268a0f0ba853f603af024d0d3
Daniel Blagy <daniel.blagy@gmail.com>
2021-10-06 18:57:20 +0300

    added profile picture loaded from github in ProfileDisplay

--- src/components/ProfileDisplay.js
+++ src/components/ProfileDisplay.js
@@ -1,7 +1,26 @@
+import {useState, useEffect} from 'react'
+
 function ProfileDisplay() {
+  const [avatarUrl, setAvatarUrl] = useState('')
+  
+  useEffect(() => {
+    fetch('http://api.github.com/users/danielblagy')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      setAvatarUrl(data.avatar_url)
+    });
+  }, [])
+  
   return (
     <div className='ProfileDisplay'>
-      <p>ProfileDisplay</p>
+      <p>
+        <img src={avatarUrl} width='250px' alt='Profile'/>
+      </p>
+      
+      <p>
+        Daniel Blagy
+      </p>
     </div>
   )
 }
