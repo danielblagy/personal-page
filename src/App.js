21e4fe1d9af687d4fddbbfd04bf1e606b4ade7bc
Daniel Blagy <daniel.blagy@gmail.com>
2021-10-06 18:26:34 +0300

    added root components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+import ProfileDisplay from './components/ProfileDisplay'
+import Navigation from './components/Navigation'
+import Content from './components/Content'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+  <div className='App'>
+    <ProfileDisplay />
+    <Navigation />
+    <Content />
+  </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/ProfileDisplay.js
@@ -0,0 +1,9 @@
+function ProfileDisplay() {
+  return (
+    <div className='ProfileDisplay'>
+      <p>ProfileDisplay</p>
+    </div>
+  )
+}
+
+export default ProfileDisplay
