514609633ef28337caf14eb0a5aee162476342ba
Daniel Blagy <daniel.blagy@gmail.com>
2021-10-06 20:07:20 +0300

    added fade content test

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,27 @@
+import {useState} from 'react'
+
 function Navigation() {
-	return (
-	  <div className='Navigation'>
-		<p>Navigation</p>
-	  </div>
-	)
+  
+  const [fadeProp, setFadeProp] = useState({fade: 'fade-out'})
+  
+  function fadeTest() {
+    if (fadeProp.fade === 'fade-in') {
+      setFadeProp({fade: 'fade-out'})
+    } else {
+        setFadeProp({fade: 'fade-in'})
+    }
+  }
+  
+  return (
+    <div className='Navigation'>
+    <p>
+      <button onClick={fadeTest}>Toggle Fade</button>
+    </p>
+    <p className={fadeProp.fade}>
+      Text
+    </p>
+    </div>
+  )
   }
   
   export default Navigation
