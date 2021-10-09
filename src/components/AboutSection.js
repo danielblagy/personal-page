ac3bb4734ec725bf974aeadc0597e590c3502454
Daniel Blagy <daniel.blagy@gmail.com>
2021-10-09 22:01:55 +0300

    added content to the about section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,9 +1,74 @@
 function AboutSection() {
-	return (
-	  <div className='AboutSection'>
-		AboutSection
-	  </div>
-	)
-  }
+  return (
+    <div className='AboutSection'>
+      <div style={{display: 'inline-block', textAlign: 'left'}}>
+        <p>
+          there’s a bluebird in my heart that<br/>
+          wants to get out<br/>
+          but I’m too tough for him,<br/>
+          I say, stay in there, I’m not going<br/>
+          to let anybody see<br/>
+          you.<br/>
+        </p>
+        <p>
+          there’s a bluebird in my heart that<br/>
+          wants to get out<br/>
+          but I pour whiskey on him and inhale<br/>
+          cigarette smoke<br/>
+          and the whores and the bartenders<br/>
+          and the grocery clerks<br/>
+          never know that<br/>
+          he’s<br/>
+          in there.<br/>
+        </p>
+        <p>
+          there’s a bluebird in my heart that<br/>
+          wants to get out<br/>
+          but I’m too tough for him,<br/>
+          I say,<br/>
+          stay down, do you want to mess<br/>
+          me up?<br/>
+          you want to screw up the<br/>
+          works?<br/>
+          you want to blow my book sales in<br/>
+          Europe?<br/>
+        </p>
+        <p>
+          there’s a bluebird in my heart that<br/>
+          wants to get out<br/>
+          but I’m too clever, I only let him out<br/>
+          at night sometimes<br/>
+          when everybody’s asleep.<br/>
+          I say, I know that you’re there,<br/>
+          so don’t be<br/>
+          sad.<br/>
+        </p>
+        <p>
+          then I put him back,<br/>
+          but he’s singing a little<br/>
+          in there, I haven’t quite let him<br/>
+          die<br/>
+          and we sleep together like<br/>
+          that<br/>
+          with our<br/>
+          secret pact<br/>
+          and it’s nice enough to<br/>
+          make a man<br/>
+          weep, but I don’t<br/>
+          weep, do<br/>
+          you?<br/>
+        </p>
+      </div>
+      
+      <br/>
+      
+      <h3 style={{display: 'inline-block', textAlign: 'right'}}>
+        Bluebird
+        <br/>
+        Charles Bukowski
+      </h3>
+    </div>
+  )
+}
   
   export default AboutSection
